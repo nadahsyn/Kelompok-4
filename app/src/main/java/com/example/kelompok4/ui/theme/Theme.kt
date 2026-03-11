6dbc2b9163b01e059675b2268fa09735a9c08080
Intan Vilodia Putri <intanintan27657@gmail.com>
2026-03-11 23:39:43 +0700

    Add profile pagee UI - Vilo

--- app/src/main/java/com/example/kelompok4/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kelompok4/ui/theme/Theme.kt
@@ -12,16 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BubblegumPink,
+    secondary = PinkSecrets,
+    tertiary = StrawberryMilkshake
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    primary = BubblegumPink,
+    secondary = PinkSecrets,
+    tertiary = StrawberryMilkshake
+)
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -31,7 +31,7 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
-)
+
 
 @Composable
 fun Kelompok4Theme(
