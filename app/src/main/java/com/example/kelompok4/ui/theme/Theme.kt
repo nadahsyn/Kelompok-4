a8ac56756a855d786ae60e8f30c2e463f8cd4d4c
nadahsyn <nadahusayna2@gmail.com>
2026-03-11 23:11:09 +0700

    tema n login

--- app/src/main/java/com/example/kelompok4/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kelompok4/ui/theme/Theme.kt
@@ -10,17 +10,26 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BubblegumPink,
+    secondary = PinkSecrets,
+    tertiary = InLove,
+    background = HumblePink,
+    surface = InLove,
+    onPrimary = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = BubblegumPink,
+    secondary = PinkSecrets,
+    tertiary = InLove,
+    background = AlmostPink,
+    surface = StrawberryMilkshake,
+    onPrimary = Color.White,
+    onBackground = HumblePink
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
