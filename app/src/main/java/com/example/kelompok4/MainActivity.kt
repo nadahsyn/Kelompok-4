16b6f51d8999eba6ca22ed3beca52e28d7ef4fd8
rayhanarzet <reyyyhnz@gmail.com>
2026-03-12 01:06:09 +0700

    dah bener

--- app/src/main/java/com/example/kelompok4/MainActivity.kt
+++ app/src/main/java/com/example/kelompok4/MainActivity.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.example.kelompok4.ui.theme.Kelompok4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             Kelompok4Theme {
-                    LoginScreen()
+                RegisterScreen()
             }
         }
     }
--- app/src/main/java/com/example/kelompok4/ui/theme/Color.kt
+++ app/src/main/java/com/example/kelompok4/ui/theme/Color.kt
@@ -2,6 +2,12 @@ package com.example.kelompok4.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val HumblePink = Color(0xFF4A202A)
+val InLove = Color(0xFF83394A)
+val BubblegumPink = Color(0xFFE4879E)
+val PinkSecrets = Color(0xFFEEAAC3)
+val StrawberryMilkshake = Color(0xFFF5C0D3)
+val AlmostPink = Color(0xFFFCD6E3)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
