60f42934e5ca15522051775927fdcfe8cb45f8f4
nadahsyn <nadahusayna2@gmail.com>
2026-03-13 00:08:22 +0700

    Update LoginScreen.kt

--- app/src/main/java/com/example/kelompok4/LoginScreen.kt
+++ app/src/main/java/com/example/kelompok4/LoginScreen.kt
@@ -4,25 +4,22 @@ import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.input.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.kelompok4.ui.theme.*
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.example.kelompok4.ui.theme.*
 
 @Composable
 fun LoginScreen(navController: NavController) {
@@ -39,11 +36,7 @@ fun LoginScreen(navController: NavController) {
             .fillMaxSize()
             .background(AlmostPink)
             .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = {
-                        keyboardController?.hide()
-                    }
-                )
+                detectTapGestures { keyboardController?.hide() }
             }
             .padding(24.dp)
             .imePadding(),
@@ -71,9 +64,7 @@ fun LoginScreen(navController: NavController) {
             onValueChange = { username = it },
             label = { Text("Username") },
             singleLine = true,
-            leadingIcon = {
-                Icon(Icons.Default.Person, contentDescription = "Username")
-            },
+            leadingIcon = { Icon(Icons.Default.Person, null) },
             colors = OutlinedTextFieldDefaults.colors(
                 focusedContainerColor = StrawberryMilkshake,
                 unfocusedContainerColor = StrawberryMilkshake,
@@ -93,23 +84,17 @@ fun LoginScreen(navController: NavController) {
             visualTransformation =
                 if (passwordVisible) VisualTransformation.None
                 else PasswordVisualTransformation(),
-            leadingIcon = {
-                Icon(Icons.Default.Lock, contentDescription = "Password")
-            },
+            leadingIcon = { Icon(Icons.Default.Lock, null) },
             trailingIcon = {
+
                 val image =
                     if (passwordVisible)
-                        Icons.Filled.Visibility
+                        Icons.Default.Visibility
                     else
-                        Icons.Filled.VisibilityOff
+                        Icons.Default.VisibilityOff
 
-                IconButton(onClick = {
-                    passwordVisible = !passwordVisible
-                }) {
-                    Icon(
-                        imageVector = image,
-                        contentDescription = "Toggle password visibility"
-                    )
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(image, null)
                 }
             },
             colors = OutlinedTextFieldDefaults.colors(
@@ -129,12 +114,15 @@ fun LoginScreen(navController: NavController) {
                 keyboardController?.hide()
 
                 if (username.isEmpty() || password.isEmpty()) {
+
                     Toast.makeText(
                         context,
                         "Username dan Password harus diisi",
                         Toast.LENGTH_SHORT
                     ).show()
+
                 } else {
+
                     Toast.makeText(
                         context,
                         "Login berhasil",
@@ -144,6 +132,11 @@ fun LoginScreen(navController: NavController) {
                     navController.navigate("profile")
                 }
             },
+            shape = RoundedCornerShape(30.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = BubblegumPink,
+                contentColor = Color.White
+            ),
             modifier = Modifier
                 .fillMaxWidth()
                 .height(50.dp)
@@ -164,9 +157,7 @@ fun LoginScreen(navController: NavController) {
         Spacer(modifier = Modifier.height(16.dp))
 
         TextButton(
-            onClick = {
-                navController.navigate("register")
-            }
+            onClick = { navController.navigate("register") }
         ) {
             Text(
                 text = "Belum punya akun? Register",
