55dfbbbb588e4e1b6e638aee4d034c703779ced4
nadahsyn <nadahusayna2@gmail.com>
2026-03-13 00:06:32 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/kelompok4/MainActivity.kt
+++ app/src/main/java/com/example/kelompok4/MainActivity.kt
@@ -1,23 +1,46 @@
 package com.example.kelompok4
 
-import AppNavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.*
 import com.example.kelompok4.ui.theme.Kelompok4Theme
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.kelompok4.ui.screens.ProfileScreen
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         setContent {
             Kelompok4Theme {
-                RegisterScreen()
+
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "login"
+                ) {
+
+                    composable("login") {
+                        LoginScreen(navController)
+                    }
+
+                    composable("register") {
+                        RegisterScreen(navController)
+                    }
+
+                    composable("profile") {
+                        ProfileScreen(navController)
+                    }
+
+                    composable("avatar") {
+                        AvatarScreen()
+                    }
+                }
             }
-            val navController = rememberNavController()
-            AppNavigation(navController)
         }
     }
 }
